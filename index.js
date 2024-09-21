1c3f3d11a71b6b58206bb94c82bfa51ad0f5d3e5
dolapo oluwole <adefisayomioluwole@gmail.com>
2024-09-21 13:29:31 +0100

    updated app

--- index.js
+++ index.js
@@ -1,35 +1,52 @@
+require("dotenv").config();
 const express = require("express");
-const { scrapeLogic } = require("./scrapeLogic");
 const app = express();
-const cors = require("cors"); // Import cors
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const { downloadPdf } = require("./downloadPdf");
+const jwt = require('jsonwebtoken');
 
 const PORT = process.env.PORT || 4000;
 
-// Enable CORS for requests from localhost:3000
+// Enable CORS for requests from specific origins
 app.use(cors({
-  origin: ["http://localhost:3000", "https://qweek.vercel.app", "https://puppet-3wt0.onrender.com"], // Allow requests from localhost:3000
+  origin: ["http://localhost:3000", "https://qweek.vercel.app", "https://make-pdf.onrender.com"], // Allow specific origins
   methods: "GET,POST", // Allow specific methods
-  credentials: true // Allow cookies or credentials to be included
+  credentials: true // Allow credentials (cookies)
 }));
+
 app.use(cookieParser());
+app.use(express.json()); // Parse JSON bodies
 
-// Middleware to parse JSON bodies
-app.use(express.json());
+// Token verification middleware
+app.use(async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).send({ success: false, message: 'Missing or invalid token' });
+    }
+    const token = authHeader.split(" ")[1];
+    const tokenValid = jwt.verify(token, process.env.TOKEN_SECRET);
 
-app.get("/scrape", async (req, res) => {
-  res.send( await scrapeLogic() )
+    if (!tokenValid) {
+      return res.status(401).send({ success: false, message: 'Invalid token' });
+    }
+
+    next(); // Pass control to the next middleware or route handler
+  } catch (error) {
+    return res.status(401).send({ success: false, message: 'Token verification failed' });
+  }
 });
 
-app.get("/", (req, res) => {
-  res.send("Render Puppeteer server is up and running!");
-});
-
+// Route for generating PDFs
 app.post('/resume', async (req, res) => {
-  const { url, selectedFont, size, isDarkMode } = req.body;
-  const result = await downloadPdf({ url, selectedFont, size, isDarkMode })
-  res.send(result)
+  try {
+    const { url, selectedFont, size, isDarkMode } = req.body;
+    const result = await downloadPdf({ url, selectedFont, size, isDarkMode });
+    res.send(result);
+  } catch (error) {
+    res.status(500).send({ success: false, message: 'Error generating PDF' });
+  }
 });
 
 app.listen(PORT, () => {
